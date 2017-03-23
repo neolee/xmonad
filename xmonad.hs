ba916bf60b33f6f464cc3f830b84f0be7b6ce734
Neo Lee <neo.lee@gmail.com>
2017-03-23 18:47:01 +0800

    Fine tuned

--- xmonad.hs
+++ xmonad.hs
@@ -14,30 +14,38 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import qualified GHC.IO.Handle.Types as H
 
-crLogHook h = do
-	dynamicLogWithPP $ tryPP h
+-- Basic
+myNormalBorderColor = bgColor
+myFocusBorderColor = "#ee0000"
+myModMask = mod1Mask
+myTerminal = "gnome-terminal"
+myBorderWidth = 1
 
-tryPP :: Handle -> PP
-tryPP h = defaultPP
+myLogHook h = do
+	dynamicLogWithPP $ myPP h
+
+myPP :: Handle -> PP
+myPP h = defaultPP
 	{ ppOutput 		= hPutStrLn h
-	, ppCurrent	    = dzenColor col1 col2 . pad . wrap space space
-	, ppVisible	    = dzenColor col6 col2 . pad . wrap space space
-	, ppHidden	    = dzenColor col6 col2 . pad . wrap space space
-	, ppHiddenNoWindows = dzenColor col6 col2 . pad . wrap space space
+	, ppCurrent	    = dzenColor dzenFGColor dzenBGColor . pad . wrap space space
+	, ppVisible	    = dzenColor highlightColor dzenBGColor . pad . wrap space space
+	, ppHidden	    = dzenColor highlightColor dzenBGColor . pad . wrap space space
+	, ppHiddenNoWindows = dzenColor highlightColor dzenBGColor . pad . wrap space space
 	, ppWsSep	    = ""
 	, ppSep		    = ""
-	, ppLayout 		= wrap "^ca(1,xdotool key alt+space)" "^ca()" . dzenColor col1 col4 . pad . wrap space space .
+	, ppLayout 		= wrap "^ca(1,xdotool key alt+space)" "^ca()" . dzenColor dzenFGColor highlightColor . pad . wrap space space .
 	( \t -> case t of
-	"Spacing 0 Grid"		-> dir_icon ++ "grid.xbm)"
-	"Spacing 0 Tall"		-> dir_icon ++ "sptall.xbm)"
-	"Mirror Spacing 0 Tall"	-> dir_icon ++ "mptall.xbm)"
-	"Full"					-> dir_icon ++ "full.xbm)"
+	"Spacing 0 Grid"		-> cmdLayoutIcon ++ "grid.xbm)"
+	"Spacing 0 Tall"		-> cmdLayoutIcon ++ "sptall.xbm)"
+	"Mirror Spacing 0 Tall"	-> cmdLayoutIcon ++ "mptall.xbm)"
+	"Full"					-> cmdLayoutIcon ++ "full.xbm)"
 	)					
 	, ppOrder  		= \(ws:l:t:_) -> [l,ws]
 	}
+cmdLayoutIcon = "^ca(1,xdotool key alt+space)^i(/home/neo/.xmonad/icons/"
 
-crWorkspace :: [String]
-crWorkspace = clickable . (map dzenEscape) $ [ "Main"
+myWorkspace :: [String]
+myWorkspace = clickable . (map dzenEscape) $ [ "Main"
 	, "Learn"
 	, "Misc"
 	--, "よん"
@@ -50,46 +58,52 @@ crWorkspace = clickable . (map dzenEscape) $ [ "Main"
 		(i,ws) <- zip [1..] l,
 		let n = i ]
 
-crKeys = [ ((mod1Mask, xK_p), spawn dmenu_cr) 
+myKeys = [ ((mod1Mask, xK_p), spawn dmenu) 
 	, ((mod1Mask, xK_q), spawn "killall dzen2; xmonad --recompile; xmonad --restart")
     , ((mod1Mask, xK_v), spawn "firefox")]
 
-crLayout = avoidStruts $ smartBorders ( sTall ||| Mirror sTall ||| sGrid ||| Full )
+myLayout = avoidStruts $ smartBorders ( myTall ||| Mirror myTall ||| myGrid ||| Full )
 	where 
-	 sTall = spacing 0 $ Tall 3 (1/2) (1/2)
-	 sGrid = spacing 0 $ Grid
+	 myTall = spacing 0 $ Tall 1 (3/100) (1/2)
+	 myGrid = spacing 0 $ Grid
 
-crDocks = composeAll 
-	[ className =? "Gimp" --> doFloat
+myDocks = composeAll 
+	[ className =? "code" --> doFloat
 	, className =? "firefox" --> doFullFloat 
 	, className =? "google-chrome" --> doFullFloat 
 	]
 
-main = do
-	barAtas <- spawnPipe bar1
-	barAtasKanan <- spawnPipe bar2
-	xmonad $ defaultConfig
-	 { manageHook = crDocks <+> manageDocks <+> manageHook defaultConfig
-	 , layoutHook = crLayout
-	 , modMask = mod1Mask
-	 , workspaces = crWorkspace
-	 , terminal = "gnome-terminal"
-	 , focusedBorderColor = "#ee0000"
-	 , normalBorderColor = col2
-	 , borderWidth = 1
-	 -- About "LG3D" see http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-SetWMName.html for detail
-	 , startupHook = spawnOnce "sh /home/neo/.xmonad/scripts/autoload.sh" <+> setWMName "LG3D"
-	 , logHook = crLogHook barAtas
-	 } `additionalKeys` crKeys
+-- About "LG3D" see http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-SetWMName.html for detail
+myStartupHook = spawnOnce "sh /home/neo/.xmonad/scripts/startup.sh"
+				<+> setWMName "LG3D"
 
+main = do
+	barAtas <- spawnPipe dzenBar
+	barAtasKanan <- spawnPipe dzenBarKanan
+	xmonad $ defaultConfig
+		{ manageHook = myDocks <+> manageDocks <+> manageHook defaultConfig
+		, layoutHook = myLayout
+		, modMask = myModMask
+		, workspaces = myWorkspace
+		, terminal = myTerminal
+		, focusedBorderColor = myFocusBorderColor
+		, normalBorderColor = myNormalBorderColor
+		, borderWidth = myBorderWidth
+		, startupHook = myStartupHook
+		, logHook = myLogHook barAtas
+		} `additionalKeys` myKeys
+
+-- Utilities
 space = "   "
-col1 = "#fcfcfc"
-col2 = "#2d2d2d"
-col3 = "#d2d2d2"
-col4 = "#F33C5B"
-col5 = "#010101"
-col6 = "#7d7d7d"
-bar1 = "dzen2 -p -ta l -e 'button3=' -fn 'Pragmata-8:bold' -fg '" ++ col1 ++ "' -bg '" ++ col2 ++ "' -h 24 -w 750"
-bar2 = "sh /home/neo/.xmonad/scripts/dzen2info.sh"
-dir_icon = "^ca(1,xdotool key alt+space)^i(/home/neo/.xmonad/icons/"
-dmenu_cr = "dmenu_run -b -nb '#2d2d2d' -sb '#F33C58'"
+bgColor = "#2d2d2d"
+highlightColor = "#7d7d7d"
+font = "Pragmata"
+
+dzenFGColor = "#fcfcfc"
+dzenBGColor = bgColor
+dzenBar = "dzen2 -p -ta l -e 'button3=' -fn '" ++ font ++ "-8:bold' -fg '" ++ dzenFGColor ++ "' -bg '" ++ dzenBGColor ++ "' -h 24 -w 750"
+dzenBarKanan = "conky -c ~/.xmonad/scripts/conky.conf | dzen2 -p -ta r -e 'button3=' -fn 'Pragmata-8' -fg '" ++ dzenFGColor ++ "' -bg '" ++ dzenBGColor ++ "' -h 24 -w 1280 -x 750"
+
+dmenuFGColor = "#F33C58"
+dmenuBGColor = bgColor
+dmenu = "dmenu_run -b -nb '" ++ dmenuBGColor ++ "' -sb '" ++ dmenuFGColor ++ "'"
